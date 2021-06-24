39a79f962877c253e960a78cfd20b8641a0b1205
ryo27-jp <calvee.20@gmail.com>
2021-06-24 20:51:21 +0900

    teamsコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :teams
   root to: 'home#index'
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
