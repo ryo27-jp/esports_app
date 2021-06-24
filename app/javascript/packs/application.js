13e97260506efb7a220ca6df460a271292150e0d
ryo27-jp <65845955+ryo27-jp@users.noreply.github.com>
2021-06-24 11:56:55 +0900

    Revert "サインアップ　サインイン機能"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import '@fortawesome/fontawesome-free/js/all'
-import 'bootstrap'
-import 'jquery'
-import '../stylesheets/application'
-
 import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-
 Rails.start()
+Turbolinks.start()
 ActiveStorage.start()
