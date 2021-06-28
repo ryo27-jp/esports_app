ae214e64bdb39caf181062eef37cb12898479d7b
ryo27-jp <calvee.20@gmail.com>
2021-06-28 13:00:15 +0900

    seedファイル作成と細かな修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+  team = Team.create(name: Faker::Game.title,
+                     about: Faker::Game.genre)
+  
+  team.image.attach(
+    io: File.open('db/fixtures/犬.jpg'),
+    filename: '犬.jpg')
+end
+
+
