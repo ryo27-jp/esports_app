f0adb1fa074bb0e794294c686addfd0651e07f6e
ryo27-jp <calvee.20@gmail.com>
2021-06-29 12:23:29 +0900

    テスト修正

--- spec/system/teams_spec.rb
+++ spec/system/teams_spec.rb
@@ -6,22 +6,22 @@ RSpec.describe "Teams", type: :system do
       @team = create(:team)
     end
 
-    it "nameがなければ登録できない" do
+    it "チーム名がなければ登録できない" do
       visit new_team_path
-      fill_in 'Name', with: ''
-      fill_in 'About', with: '作れるんです'
+      fill_in 'チーム名', with: ''
+      fill_in 'チーム概要', with: '作れるんです'
       attach_file "team[image]", "db/fixtures/犬.jpg"
-      click_button 'Create Team'
+      click_button '登録する'
 
       expect(page).to have_content '登録に失敗しました。'
     end
 
-    it "nameはユニークである事" do
+    it "チーム名はユニークである事" do
       visit new_team_path
-      fill_in 'Name', with: 'testチーム'
-      fill_in 'About', with: '作れるんです'
+      fill_in 'チーム名', with: 'testチーム'
+      fill_in 'チーム概要', with: '作れるんです'
       attach_file "team[image]", "db/fixtures/犬.jpg"
-      click_button 'Create Team'
+      click_button '登録する'
 
       expect(page).to have_content '登録に失敗しました。'
     end
@@ -30,10 +30,10 @@ RSpec.describe "Teams", type: :system do
   describe "作成出来る" do
     it "teamが作成出来る事" do
       visit new_team_path
-      fill_in 'Name', with: 'これは作れる'
-      fill_in 'About', with: '作れるんです'
+      fill_in 'チーム名', with: 'これは作れる'
+      fill_in 'チーム概要', with: '作れるんです'
       attach_file "team[image]", "db/fixtures/犬.jpg"
-      click_button 'Create Team'
+      click_button '登録する'
 
       expect(page).to have_content '登録しました。'
     end
