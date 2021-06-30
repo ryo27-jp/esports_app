7a64129003e346767660341ddff1707fffc1b655
ryo27-jp <calvee.20@gmail.com>
2021-06-30 23:55:05 +0900

    DivisionCRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :teams
+  resources :teams do
+    resources :divisions, except: [:index], shallow: true
+  end
+
   root to: 'home#index'
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
