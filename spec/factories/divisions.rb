0ee6af97a9b98f7126bfec58e6108a2defca303c
ryo27-jp <calvee.20@gmail.com>
2021-07-01 08:47:49 +0900

    divisions system_test作成

--- spec/factories/divisions.rb
+++ spec/factories/divisions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :division do
-    name { "MyString" }
-    description { "MyString" }
-    team { nil }
+    name { 'test部門' }
+    description { '部門について書くところです。' }
+    team
   end
 end
---
+++ spec/system/divisions_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe "Divisions", type: :system do
+  let(:team) { create(:team, name: 'テスト') }
+  
+  describe '作成できない' do
+    let!(:division){create(:division)}
+ 
+    it '部門名がなければ登録できない' do
+      visit new_team_division_path(team)
+      fill_in '部門名', with: ''
+      fill_in '部門概要', with: '作れないです'
+      attach_file 'division[image]', 'db/fixtures/犬.jpg'
+      click_button '登録する'
+
+      expect(page).to have_content '部門名を入力してください'
+    end
+
+    it '部門名はユニークである事' do
+      visit new_team_division_path(team)
+      fill_in '部門名', with: 'test部門'
+      fill_in '部門概要', with: '作れないです'
+      attach_file 'division[image]', 'db/fixtures/犬.jpg'
+      click_button '登録する'
+
+      expect(page).to have_content '登録に失敗しました。'
+    end
+  end
+
+  describe '作成出来る' do
+    it 'divisionが作成出来る事' do
+      visit new_team_division_path(team)
+      fill_in '部門名', with: 'これは作れる'
+      fill_in '部門概要', with: '作れるんです'
+      attach_file 'division[image]', 'db/fixtures/犬.jpg'
+      click_button '登録する'
+
+      expect(page).to have_content '登録しました。'
+    end
+  end
+end
