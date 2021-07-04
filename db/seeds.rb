85a11b969ebfc06a61c959e48a61ee93ebeea10b
ryo27-jp <calvee.20@gmail.com>
2021-07-04 16:28:06 +0900

    ページネーション実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-20.times do
+20.times do |n|
   team = Team.create(name: Faker::Game.title,
                      about: Faker::Game.genre)
 
@@ -6,4 +6,14 @@
     io: File.open('db/fixtures/犬.jpg'),
     filename: '犬.jpg'
   )
+
+  division = Division.create(name: Faker::Sports::Football.team,
+                  description: Faker::Sports::Football.coach,
+                  team_id: team.id  )
+
+  Member.create(name: Faker::Music::Hiphop.artist,
+                url: "https://google.com/#{n}",
+                biography: Faker::Music::Hiphop.subgenres,
+                division_id: division.id  )
+                
 end
