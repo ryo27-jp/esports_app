76603383c54d18e033ce147c013484901cf7797f
ryo27-jp <calvee.20@gmail.com>
2021-07-09 17:27:13 +0900

    memberとtagも

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,7 +2,7 @@ class Member < ApplicationRecord
   belongs_to :division
   has_one_attached :image
 
-  has_many :members_tags
+  has_many :members_tags, dependent: :destroy
   has_many :tags, through: :members_tags
 
   validates :name, presence: true, uniqueness: true
