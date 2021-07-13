bc6b29ed69c4dd371a203c88ee3d6ecca7136ec9
ryo27-jp <calvee.20@gmail.com>
2021-07-13 13:44:01 +0900

    検索結果のviewを調整

--- spec/system/search.spec.rb
+++ spec/system/search.spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Search', type: :system do
         select 'チーム名', from: 'search_form[choice]'
         fill_in 'search_form[name]', with: ''
         click_button '検索'
-        expect(page).to have_content "チーム名: #{Team.first.name}"
+        expect(page).to have_content "#{Team.first.name}"
       end
     end
 
@@ -19,7 +19,7 @@ RSpec.describe 'Search', type: :system do
         select 'メンバー', from: 'search_form[choice]'
         fill_in 'search_form[name]', with: ''
         click_button '検索'
-        expect(page).to have_content "チーム名: #{Member.first.name}"
+        expect(page).to have_content "#{Member.first.name}"
       end
     end
   end
