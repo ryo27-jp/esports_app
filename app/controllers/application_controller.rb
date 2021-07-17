e5054c0239a99ca5d3df1ac3a4b9be8a73447043
ryo27-jp <calvee.20@gmail.com>
2021-07-17 10:53:42 +0900

    エラーハンドリングのコントローラーを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   before_action :set_search
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+  
+  include ErrorHandlers if Rails.env.production?
+
   private
 
   def set_search
     @search_form = SearchForm.new
   end
+
 end
