672efb0d3fc5f7b454bad68e42e720c578005d5d
ryo27-jp <calvee.20@gmail.com>
2021-08-02 23:33:31 +0900

    tags_showにページネーション追加

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,13 @@
 class TagsController < ApplicationController
+  include Pagy::Backend
   def index
     @tags = Tag.all
   end
 
   def show
     @tag = Tag.find(params[:id])
+    @dpagy, @divisions = pagy(@tag.divisions,page_param: :dpage, items: 8)
+    @mpagy, @members = pagy(@tag.members,page_param: :mpage, items: 8)
   end
   
   def new
